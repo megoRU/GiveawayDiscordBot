d94478917f651faf1bdc461237d8d4b4904ea645
mego <savin.yura2014@gmail.com>
2021-03-20 00:16:11 +0300

    Version: 1.3.0 1. Fix Reactions 2. Update Dependency

--- src/main/java/giftaway/Reactions.java
+++ src/main/java/giftaway/Reactions.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEve
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import startbot.BotStart;
+import startbot.Statcord;
 
 public class Reactions extends ListenerAdapter {
 
@@ -12,57 +13,54 @@ public class Reactions extends ListenerAdapter {
 
   @Override
   public void onGuildMessageReactionAdd(@NotNull GuildMessageReactionAddEvent event) {
-    try {
-      if (event.getUser().isBot()) {
-        return;
+
+    if (event.getUser().isBot()) {
+      return;
+    }
+
+    if (BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) == null) {
+      return;
+    }
+
+    if (!event.getGuild().getSelfMember()
+        .hasPermission(event.getChannel(), Permission.MESSAGE_WRITE)) {
+      return;
+    }
+
+    if (!event.getGuild().getSelfMember()
+        .hasPermission(event.getChannel(), Permission.MESSAGE_MANAGE)) {
+      event.getChannel().sendMessage("Bot: `" + event.getGuild().getSelfMember().getUser().getName()
+          + "` don\\`t have: `Permission.MESSAGE_MANAGE` \nTo work correctly, add the specified permission to it!")
+          .queue();
+      return;
+    }
+
+    //Что делает этот код...:
+    if (!event.getReactionEmote().isEmoji()) {
+      event.getReaction().removeReaction(event.getUser()).queue();
+      return;
+    }
+    //
+
+    String emoji = event.getReactionEmote().getEmoji();
+
+    if (!(emoji.equals(emojiPresent))
+        && BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) != null) {
+      event.getReaction().removeReaction(event.getUser()).queue();
+    }
+
+    long guild = event.getGuild().getIdLong();
+    Gift gift;
+    gift = new Gift();
+
+    if (emoji.equals(emojiPresent)
+        && BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) != null
+        && gift.hasGift(guild)) {
+      gift = gift.getGift(event.getGuild().getIdLong());
+      if (gift.getListUsersHash(event.getUser().getId()) == null) {
+        gift.addUserToPoll(event.getMember().getUser(), event.getGuild(), event.getChannel());
+        Statcord.commandPost("gift", event.getUser().getId());
       }
-
-      if (BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) == null) {
-        return;
-      }
-
-      if (!event.getGuild().getSelfMember()
-          .hasPermission(event.getChannel(), Permission.MESSAGE_WRITE)) {
-        return;
-      }
-
-      if (!event.getGuild().getSelfMember()
-          .hasPermission(event.getChannel(), Permission.MESSAGE_MANAGE)) {
-        event.getChannel().sendMessage("Bot: `" + event.getGuild().getSelfMember().getUser().getName()
-            +"` don\\`t have: `Permission.MESSAGE_MANAGE` \nTo work correctly, add the specified permission to it!").queue();
-        return;
-      }
-
-      //Что делает этот код...:
-      if (!event.getReactionEmote().isEmoji()) {
-        event.getReaction().removeReaction(event.getUser()).queue();
-        return;
-      }
-      //
-
-      String emoji = event.getReactionEmote().getEmoji();
-
-      if (!(emoji.equals(emojiPresent))
-          && BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) != null) {
-        event.getReaction().removeReaction(event.getUser()).queue();
-      }
-
-      long guild = event.getGuild().getIdLong();
-      Gift gift;
-      gift = new Gift();
-
-      if (emoji.equals(emojiPresent)
-          && BotStart.getIdMessagesWithGiveawayEmoji().get(event.getMessageId()) != null
-          && gift.hasGift(guild)) {
-        gift = gift.getGift(event.getGuild().getIdLong());
-        if (gift.getListUsersHash(event.getUser().getId()) == null) {
-          gift.addUserToPoll(event.getMember().getUser(), event.getGuild(), event.getChannel());
-        }
-      }
-
-    } catch (IllegalStateException e) {
-      e.printStackTrace();
     }
   }
-
 }
