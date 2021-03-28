52ca0478b02b692f66815550c1b26553c7997858
mego <savin.yura2014@gmail.com>
2021-03-28 13:22:03 +0300

    Version: 1.6.0 1. Moved from Main to BotStart

--- src/main/java/startbot/BotStart.java
+++ src/main/java/startbot/BotStart.java
@@ -2,6 +2,8 @@ package startbot;
 
 import config.Config;
 import events.MessageWhenBotJoinToGuild;
+import giveaway.Gift;
+import giveaway.GiveawayRegistry;
 import giveaway.MessageGift;
 import giveaway.Reactions;
 import java.sql.Connection;
@@ -19,12 +21,22 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class BotStart {
-
+  //TODO: Реализовать авто-завершение Giveaways по таймеру, также под это реализовать логику в Gift и MessageGift
   private static JDA jda;
   private final JDABuilder jdaBuilder = JDABuilder.createDefault(Config.getTOKEN());
   private static final Map<String, String> mapPrefix = new HashMap<>();
+  private static final Map<Integer, String> guildIdHashList = new HashMap<>();
 
   public void startBot() throws Exception {
+    //Получаем id guild и id message
+    getMessageIdFromDB();
+
+    //Получаем всех участников по гильдиям
+    getUsersWhoTakePartFromDB();
+
+    //Получаем все префиксы из базы данных
+    getPrefixFromDB();
+
     jdaBuilder.setAutoReconnect(true);
     jdaBuilder.setStatus(OnlineStatus.ONLINE);
     jdaBuilder.setActivity(Activity.playing("—> !help"));
@@ -48,8 +60,99 @@ public class BotStart {
         mapPrefix.put(rs.getString("serverId"), rs.getString("prefix"));
       }
 
-      rs.close();
       connection.close();
+      rs.close();
+      statement.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void getMessageIdFromDB() {
+    try {
+      Connection connection = DriverManager.getConnection(Config.getPrefixConnection(), Config.getPrefixUser(), Config.getPrefixPass());
+      Statement statement = connection.createStatement();
+      String sql = "select * from ActiveGiveaways";
+      ResultSet rs = statement.executeQuery(sql);
+      while (rs.next()) {
+
+        long guild_long_id = rs.getLong("guild_long_id");
+        long message_id_long = rs.getLong("message_id_long");
+//        System.out.println("guild id:" + guild_long_id + " id: сообщения: " + message_id_long);
+        //get end date
+
+        guildIdHashList.put(guildIdHashList.size() + 1, String.valueOf(guild_long_id));
+
+        GiveawayRegistry.getInstance().setGift(guild_long_id, new Gift(guild_long_id));
+        GiveawayRegistry.getInstance().getMessageId().put(guild_long_id, String.valueOf(message_id_long));
+        GiveawayRegistry.getInstance().getIdMessagesWithGiveawayEmoji().put(guild_long_id, String.valueOf(message_id_long));
+
+      }
+      rs.close();
+      statement.close();
+      connection.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void getUsersWhoTakePartFromDB() {
+    try {
+      Connection conn = DriverManager.getConnection(Config.getGiveawayConnection(), Config.getGiveawayUser(), Config.getGiveawayPass());
+      Statement statement = conn.createStatement();
+
+      for (int i = 1; i <= guildIdHashList.size(); i++) {
+        String sql = "select * from `" + guildIdHashList.get(i) + "`;";
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+
+          long userIdLong = rs.getLong("user_long_id");
+
+          System.out.println("Guild id: " + guildIdHashList.get(i) + " user id long: " + userIdLong);
+
+          //Добавляем пользователей в hashmap
+          GiveawayRegistry.getInstance()
+              .getActiveGiveaways()
+              .get(Long.parseLong(guildIdHashList.get(i))).getListUsersHash()
+              .put(String.valueOf(userIdLong), String.valueOf(userIdLong));
+
+          //Считаем пользователей в hashmap и устанавливаем верное значение
+          GiveawayRegistry.getInstance()
+              .getActiveGiveaways()
+              .get(Long.parseLong(guildIdHashList.get(i))).getListUsers()
+              .add(String.valueOf(userIdLong));
+
+          //Устанавливаем счетчик на верное число
+          GiveawayRegistry.getInstance()
+              .getActiveGiveaways()
+              .get(Long.parseLong(guildIdHashList.get(i))).setCount(GiveawayRegistry.getInstance()
+              .getActiveGiveaways()
+              .get(Long.parseLong(guildIdHashList.get(i))).getListUsersHash().size());
+        }
+
+      }
+      statement.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    guildIdHashList.clear();
+  }
+
+  private void getPrefixFromDB() {
+    try {
+      Connection connection = DriverManager.getConnection(Config.getPrefixConnection(), Config.getPrefixUser(), Config.getPrefixPass());
+      Statement statement = connection.createStatement();
+      String sql = "select * from prefixs";
+      ResultSet rs = statement.executeQuery(sql);
+
+      while (rs.next()) {
+        mapPrefix.put(rs.getString("serverId"), rs.getString("prefix"));
+      }
+
+      connection.close();
+      rs.close();
+      statement.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
