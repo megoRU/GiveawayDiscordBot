fc843ec2899210cafd0f598515e5d893d9a09f59
mego <savin.yura2014@gmail.com>
2021-04-06 22:22:32 +0300

    Version: 2.1.1 1. Optimization for StringBuilder

--- src/main/java/giveaway/Gift.java
+++ src/main/java/giveaway/Gift.java
@@ -24,7 +24,7 @@ public class Gift {
   private final List<String> listUsers = new ArrayList<>();
   private final Map<String, String> listUsersHash = new HashMap<>();
   private final Set<String> uniqueWinners = new HashSet<>();
-  private final StringBuilder insertQuery = new StringBuilder();
+  private StringBuilder insertQuery = new StringBuilder();
   private final Random random = new Random();
   private long guildId;
   private int count;
@@ -119,7 +119,7 @@ public class Gift {
         + "VALUES" + insertQuery.toString();
     if (!insertQuery.isEmpty()) {
       DataBase.getConnection().createStatement().execute(sql);
-      insertQuery.delete(0, insertQuery.length());
+      insertQuery = new StringBuilder(); //Нужно протестировать
     }
   }
 
