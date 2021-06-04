826c88b365dbd8b79a8e2ecc5648e701f1ac2b6b
megoRU <savin.yura2014@gmail.com>
2021-06-04 23:31:50 +0300

    Version 3.0.4 1. Add auto reconnect to Database

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -6,16 +6,17 @@ public class Config {
   public static final String DEV_BOT_TOKEN2 = "";
   private static final String PRODUCTION_BOT_TOKEN = "";
   private static final String TOKEN = DEV_BOT_TOKEN;
+  private static final String GIVEAWAY_NAME = "";
 
   //Данный от БД с Giveaways
-  private static final String GIVEAWAY_CONNECTION = ""; //utf8mb4
-  private static final String GIVEAWAY_USER = "";
+  private static final String GIVEAWAY_CONNECTION = "jdbc:mysql://:3306/" + GIVEAWAY_NAME + "?useSSL=false&serverTimezone=UTC&characterEncoding=utf8&autoReconnect=true"; //utf8mb4
+  private static final String GIVEAWAY_USER = GIVEAWAY_NAME;
   private static final String GIVEAWAY_PASS = "";
 
   private static final String TOP_GG_API_TOKEN = "";
   private static final String BOT_ID = "808277484524011531"; //megoDev: 780145910764142613 //giveaway: 808277484524011531
   private static final String STATCORD = "";
-  private static final String URL = "https://discord.com/oauth2/authorize?client_id=780145910764142613&scope=bot&permissions=11328";
+  private static final String URL = "https://discord.com/oauth2/authorize?client_id=780145910764142613&scope=bot&permissions=2147511360";
 
   public static String getTOKEN() {
     return TOKEN;
