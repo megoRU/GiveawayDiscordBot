45c32a4f5095dd4db730c55aede4c66d830c1619
megoRU <savin.yura2014@gmail.com>
2024-01-04 14:35:27 +0300

    some fix

--- src/main/java/main/core/events/CheckBot.java
+++ src/main/java/main/core/events/CheckBot.java
@@ -3,7 +3,6 @@ package main.core.events;
 import main.giveaway.ChecksClass;
 import main.jsonparser.JSONParsers;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
-import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.jetbrains.annotations.NotNull;
@@ -19,12 +18,15 @@ public class CheckBot {
     public void check(@NotNull SlashCommandInteractionEvent event) {
         var guildId = Objects.requireNonNull(event.getGuild()).getId();
 
-        GuildChannelUnion textChannel = event.getOption("textchannel", OptionMapping::getAsChannel);
-        GuildChannel guildChannel = textChannel != null ? textChannel : event.getGuildChannel().asTextChannel();
-        boolean canSendGiveaway = ChecksClass.canSendGiveaway(guildChannel, event);
-        if (canSendGiveaway) {
-            String giftPermissions = String.format(jsonParsers.getLocale("gift_permissions", guildId), guildChannel.getId());
-            event.reply(giftPermissions).queue();
+        GuildChannel guildChannel = event.getOption("textchannel", OptionMapping::getAsChannel);
+        if (guildChannel == null) {
+            event.reply("Channel is Null").queue();
+        } else {
+            boolean canSendGiveaway = ChecksClass.canSendGiveaway(guildChannel, event);
+            if (canSendGiveaway) {
+                String giftPermissions = String.format(jsonParsers.getLocale("gift_permissions", guildId), guildChannel.getId());
+                event.reply(giftPermissions).queue();
+            }
         }
     }
 }
