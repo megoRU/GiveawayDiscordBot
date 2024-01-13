75ede75f9121f41439a3d3ca538163f9127a031c
megoRU <savin.yura2014@gmail.com>
2024-01-13 17:04:03 +0300

    New Update
    1. Fix

--- src/main/java/main/giveaway/GiveawayEnd.java
+++ src/main/java/main/giveaway/GiveawayEnd.java
@@ -67,7 +67,6 @@ public class GiveawayEnd {
                 notEnoughUsers.setTitle(giftNotEnoughUsers);
                 notEnoughUsers.setDescription(giftGiveawayDeleted);
                 //Отправляет сообщение
-
                 giveawayMessageHandler.editMessage(notEnoughUsers, guildId, textChannelId);
 
                 activeGiveawayRepository.deleteById(guildId);
