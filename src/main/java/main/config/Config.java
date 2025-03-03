ad35d257d78f07c46b97cfce1d424ebb02b87385
megoRU <savin.yura2014@gmail.com>
2025-03-03 15:33:16 +0300

    Fix

--- src/main/java/main/config/Config.java
+++ src/main/java/main/config/Config.java
@@ -5,8 +5,6 @@ public class Config {
     private static final String DEV_BOT_TOKEN = System.getenv("DEV_TOKEN");
     private static final String PRODUCTION_BOT_TOKEN = System.getenv("TOKEN");
     private static final String TOKEN = PRODUCTION_BOT_TOKEN;
-    private static final String BOT_ID = "808277484524011531"; //megoDev: 780145910764142613 //giveaway: 808277484524011531
-    private static final String BOTICORD = System.getenv("BOTICORD");
 
     private static volatile boolean IS_DEV = true;
 
@@ -20,15 +18,7 @@ public class Config {
         return TOKEN;
     }
 
-    public static String getBotId() {
-        return BOT_ID;
-    }
-
     public static boolean isIsDev() {
         return IS_DEV;
-    }
-
-    public static String getBoticord() {
-        return BOTICORD;
     }
 }
